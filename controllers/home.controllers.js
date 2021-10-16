63f0128df373e8361bcbc84a9221ef65031309b3
girikprabhakar <prabhakar8285@gmail.com>
2021-10-16 14:28:07 -0400

    login functionality

--- controllers/home.controllers.js
+++ controllers/home.controllers.js
@@ -1,3 +1,4 @@
+const User = require('../models/user')
 const register = (req, res, next) => {
     return res.render("register")
 }
@@ -10,8 +11,34 @@ const login = (req, res, next) => {
 const dashboard = (req, res, next) => {
     return res.render("dashboard")
 }
+
+const login_Post = async (req, res, next) => {
+    console.log(req.body);
+    const username = req.body.uname;
+        password = req.body.psw;
+        user = await User.findOne({ username })
+        if (!user) {
+            return res.render('login',{
+                message: 'Invalid username or password',
+                messageClass: 'alert-danger'
+            })
+        }
+        if (password == user.password) {
+            console.log("success")
+            res.redirect(301,"dashboard");
+        }else{
+            
+            return res.render('login',{
+                message: 'Invalid username or password',
+                messageClass: 'alert-danger'
+            })
+        }
+        
+}
+
 module.exports = {
     register,
     login,
-      dashboard,
+    dashboard,
+    login_Post
   };
--- routes/home.routes.js
+++ routes/home.routes.js
@@ -5,6 +5,6 @@ const router = express.Router();
 router.get('/register', homeControllers.register);
 router.get('/login', homeControllers.login);
 router.get('/dashboard', homeControllers.dashboard);
-
+router.post('/login', homeControllers.login_Post);
 
 module.exports = router
