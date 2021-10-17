6bd298e1936323029ab9775b20065a864a69bf58
“Dhruvin <dhruvinmakwana04@gmail.com>
2021-10-17 16:07:13 -0400

    added detection of key landmarks of human body

--- public/js/ARWorkout.js
+++ public/js/ARWorkout.js
@@ -14,18 +14,21 @@ class ARWorkoutEngine {
         this.userCanvasContext.scale(-1, 1);
         this.initializePoseNet()
         this.initializeCamera()
-
+        this.POSENET_LOADED=false
 
     }
     async initializePoseNet(){
         const detectorConfig = {modelType: poseDetection.movenet.modelType.SINGLEPOSE_LIGHTNING};
         this.detector = await poseDetection.createDetector(poseDetection.SupportedModels.MoveNet, detectorConfig);
+        this.POSENET_LOADED=true
     }
     async  initializeCamera() {
         const camera = new Camera(this.userVideo, {
             onFrame: async () => {
                 this.drawUserStream();
-                // await faceMesh.send({ image: this.augmentationCanvas });
+                if(this.POSENET_LOADED){
+                    this.performPredictions()
+                }
             },
             width: 1280,
             height: 720,
@@ -34,7 +37,19 @@ class ARWorkoutEngine {
 
     }
     async performPredictions(){
-        const poses = await this.detector.estimatePoses(image);
+        const poses = await this.detector.estimatePoses(this.userVideo);
+        console.log(poses)
+        if(poses.length>0){
+            poses[0].keypoints.forEach(element => {
+            console.log()
+            this.userCanvasContext.beginPath();
+            this.userCanvasContext.arc(element.x, element.y, 5, 0, Math.PI*2, true);
+            this.userCanvasContext.closePath();
+            this.userCanvasContext.fillStyle = 'red';
+            this.userCanvasContext.fill();
+            });
+        }
+
     }
     drawUserStream(){
         this.userCanvasContext.drawImage(this.userVideo,0,0,this.userVideo.videoWidth, this.userVideo.videoHeight)
